afaa96f12e326ac4d2b01df2d9d4026437169216
Vineeth Boppudi <65538431+vineeth2328@users.noreply.github.com>
2023-12-18 05:44:21 +0000

    prime nuber

---
+++ LeetCode/3.prime.js
@@ -0,0 +1,25 @@
+function prime(){
+
+let a = 5;
+if (a % 2 == 0 ){
+    console.log("its a prime number")
+}else{
+    console.log("its not a prime")
+}
+
+}
+prime()
+
+
+function listofprime(){
+
+    for (let i = 2; i<100; i++){
+
+       if( i % 2 == 0){
+               console.log("prime number",i)
+       }
+
+        }
+    }
+
+listofprime()
